08d81ec193606b2c1a787d6dcc371c8e9aaf6486
dittmar <589530+GeorgeDittmar@users.noreply.github.com>
2021-03-08 23:36:46 -0800

    setting up more class structures

--- deepchat/deep_chat.py
+++ deepchat/deep_chat.py
@@ -1,7 +1,28 @@
 
 import torch
 
+from deepchat.models.dialogpt_model import DialoGPT
+from deepchat.conversation import Conversation
+
 
 class DeepChat(object):
-    def __init__(self, model) -> None:
+    def __init__(self, model, model_config=None, device_type="cpu") -> None:
         super().__init__()
+        self.model = self.__load_model(model)
+        self.model_config = model_config if model_config else None
+        self.device_type = device_type
+
+    def __load_model(self, model):
+        if self.model_config:
+            return DialoGPT(self.device_type, model)
+        else:
+            return DialoGPT(self.device_type, model)
+
+    def run(self):
+        """
+            Starts the converstaion with the bot
+        """
+        pass
+
+    def get_conversation_history(self):
+        pass
--- deepchat/models/dialogpt_model.py
+++ deepchat/models/dialogpt_model.py
@@ -5,19 +5,31 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 class DialoGPT(AbstractModel):
 
-    def __init__(self, device_type, model, max_squence_len=1024, top_k=1, num_beams=5):
-        self.device_type = torch.device(device_type.lower())
+    def __init__(self, device_type, model, max_squence_len=1024, top_k=1, top_p=.8, num_beams=5):
+        self.device_type = self.__get_device(device_type)
         self.model = AutoModelForCausalLM.from_pretrained(
             model).to(self.device_type)
         self.tokenizer = AutoTokenizer.from_pretrained(model)
         self.max_sequence_len = max_squence_len
         self.top_k = top_k
+        self.top_p = top_p
         self.num_beams = num_beams
+
+    def __get_device(self, device):
+
+        if device == "cuda":
+            if not torch.cuda.is_available():
+                print("Cuda not available. Defaulting to CPU.")
+                device = "cpu"
+
+        return torch.device(device)
 
     def next_utterance(self, chat_history_ids):
         return self.model.generate(chat_history_ids,
                                    max_length=self.max_sequence_len,
                                    top_k=self.top_k,
+                                   do_sample=True,
+                                   top_p=self.top_p,
                                    num_beams=self.num_beams)
 
     def fine_tune(self, data):
