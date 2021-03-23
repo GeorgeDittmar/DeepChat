900ec60f7c54bfd47ac07d29d1b40b1052dcf03f
dittmar <589530+GeorgeDittmar@users.noreply.github.com>
2021-03-22 23:05:03 -0700

    refactoring some of the methods to the abstract class since they are shared between models

--- deepchat/models/abstract_model.py
+++ deepchat/models/abstract_model.py
@@ -1,3 +1,6 @@
+import torch
+import logging
+import transformers
 from abc import ABC, abstractmethod
 
 
@@ -7,9 +10,22 @@ class AbstractModel(ABC):
         Base abstract class for the model
     """
     @abstractmethod
-    def predict(self, chat_history):
+    def predict(self, user_input, conversation):
         raise NotImplementedError()
 
     @abstractmethod
     def fine_tune(self, data):
         raise NotImplementedError()
+
+    def __get_device(self, device):
+
+        if device == "cuda":
+            if not torch.cuda.is_available():
+                logging.info("Cuda not available. Defaulting to CPU.")
+                device = "cpu"
+
+        return torch.device(device)
+
+    def __decode_bot_response(self, bot_output, input_ids):
+        """decodes the output from the model"""
+        return self.tokenizer.decode(bot_output[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
--- deepchat/models/dialogpt_model.py
+++ deepchat/models/dialogpt_model.py
@@ -26,19 +26,6 @@ class DialoGPT(AbstractModel):
         self.top_p = top_p
         self.num_beams = num_beams
 
-    def __get_device(self, device):
-
-        if device == "cuda":
-            if not torch.cuda.is_available():
-                logging.info("Cuda not available. Defaulting to CPU.")
-                device = "cpu"
-
-        return torch.device(device)
-
-    def __decode_bot_response(self, bot_output, input_ids):
-        """decodes the output from the model"""
-        return self.tokenizer.decode(bot_output[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
-
     def get_tokenizer(self):
         return self.tokenizer
 
